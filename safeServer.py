70524f9809db5a3e90f8dcc98b5f863496d0198b
huangyuan1 <wshy1121@qq.com>
2015-05-19 19:58:19 +0800

    添加安全模块safServer.py

---
+++ safeServer.py
@@ -0,0 +1,40 @@
+from pyDes import *
+
+
+class CSafeServer():
+	"""DES encryption/decrytpion class"""
+	KEY_MAP_SIZE = 256
+	SAFE_KEY_LEN = 16
+	m_mainKey = "amwfqp1121amwfqp"
+	
+	def __memset(self, mem, num, size):
+		for i in xrange(size):
+			mem[i] = num
+			
+
+	def __init__(self):
+		keyLen = self.SAFE_KEY_LEN / 2;
+		i = 0;
+		self.m_keyMap = []
+		key = [None] * keyLen;
+		keyDes = triple_des(self.m_mainKey)
+		while (i < self.KEY_MAP_SIZE):
+			self.__memset(key, i, keyLen)
+			i+= keyLen;
+   			d = keyDes.encrypt("12345678")
+   			keyDes.decrypt(key)
+   			
+   			self.m_keyMap.extend(key)
+
+	def test(self):
+		print self.m_keyMap
+
+
+
+if __name__ == '__main__':
+    #_example_des_()
+    #_example_triple_des_()
+
+    
+    safe_server = CSafeServer();
+    safe_server.test();
